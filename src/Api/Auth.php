30726152e0ce9de29d93c7a7329392aacf2144fe
booni3 <adam@profilestudio.com>
2018-10-08 17:06:18 +0100

    tidy

--- src/Api/Auth.php
+++ src/Api/Auth.php
@@ -6,11 +6,10 @@ class Auth extends Api
 {
     public function AuthorizeByApplication()
     {
-        $res = $this->_get('Auth/AuthorizeByApplication', [
+        return $this->_get('Auth/AuthorizeByApplication', [
             "applicationId" => $this->applicationId,
             "applicationSecret" => $this->applicationSecret,
             "token" => $this->token
         ]);
-        return $res;
     }
 }
